bf18616da350e99b59f6d5a247fb4a0a4b77eb40
Axel Barragan <axelbarragan@gmail.com>
2018-05-29 18:13:26 -0500

    Cambios cap 12
    Cambios cap 12

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>LaraDex - @yield('title')</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		@yield('content')
+	</div>
+	
+</body>
+</html>
--- resources/views/trainers/create.blade.php
+++ resources/views/trainers/create.blade.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="UTF-8">
-	<title>Crear</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+@extends('layouts.app')
 
-</head>
-<body>
-	<div class="container">
-		<div class="form-group">
-			<label for="">Nombre</label>
-			<input type="text" class="form-control"></input>
-		</div>
-		<button type="submit" class="btn btn-primary">Guardar</button>
-	</div>
-	
-</body>
-</html>
+@section('title', 'Trainers Create')
+@section('content')
+<div class="form-group">
+	<label for="">Nombre</label>
+	<input type="text" class="form-control"></input>
+</div>
+<button type="submit" class="btn btn-primary">Guardar</button>
+@endsection
