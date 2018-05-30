36b813a72ffd4028b97bc4bb4865407c8bf5985c
Axel Barragan <axelbarragan@gmail.com>
2018-05-30 16:47:05 -0500

    Cambios ejercicio cap 18
    Cambios ejercicio cap 18

--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -12,7 +12,7 @@
 			<img class="card-img-top" src="/images/{{ $trainer->avatar }}" alt="Card image cap">
 			<div class="card-body">
 				<h5 class="card-title">{{ $trainer->name }}</h5>
-				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<p class="card-text">{{ $trainer->description }}</p>
 				<a href="#" class="btn btn-primary">Go somewhere</a>
 			</div>
 		</div>
